347696efb7d968dadb66bf2db537795dd5860269
tempxlacc <tempxlacc@users.noreply.github.com>
2020-06-15 18:41:58 -0700

    script

--- netfix.sh
+++ netfix.sh
@@ -1,6 +1,19 @@
+eval $(ssh-agent -s);
+ssh-add ~/.ssh/id_rsa_hav4ik;
 
-ifconfig > netinfo.txt;
-git add netinfo.txt;
-git commit -m 'update';
-git push origin master;
-sleep 2h;
+while true;
+do
+	ifconfig > netinfo.txt;
+	DIFF=$(git diff netinfo.txt);
+	if [ "$DIFF" != "" ];
+	then
+		git add netinfo.txt;
+		git commit -m 'update';
+		git push origin master;
+		echo "Repository updated with netinfo.txt";
+	else
+		echo "Nothing has changed. Repeating after 1 hour... (WARNING: DO NOT CLOSE THIS!!!)";
+	fi;
+	
+	sleep 3600s;
+done
